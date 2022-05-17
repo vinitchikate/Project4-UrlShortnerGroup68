1d0568e9f0898e1b762f82b81f90bf097bbb04ce
Kirtan-G <kirtan942003@gmail.com>
2022-05-17 15:36:01 +0530

    Kirtan Gajjar

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,1 +1,21 @@
-//
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    urlCode: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    longUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+module.exports = mongoose.model('url', urlSchema);
