3f1ba800b2b188f55ec25783914495cf7338d49e
Kirtan-G <kirtan942003@gmail.com>
2022-05-20 10:06:18 +0530

    Kirtan Gajjar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const route = require('./routes/route');
+
 const app = express();
 
 app.use(bodyParser.json());
